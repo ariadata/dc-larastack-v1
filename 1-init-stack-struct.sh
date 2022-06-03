cafa136b12c365e96d22a2060c96dc51cf6ee594
Mehrdad Amini <mehrdad@arzinja.dev>
2022-06-03 05:42:31 +0300

    update

--- 1-init-stack-struct.sh
+++ 1-init-stack-struct.sh
@@ -11,6 +11,30 @@ if [ ! -f /src/.env ]; then
 	cp ./src/.env.example ./src/.env
 fi
 
+# find UID/GID and write in .env file
+sed -i "s|UID=.*|UID=$(id -u)|g" .env
+sed -i "s|GID=.*|GID=$(id -g)|g" .env
+
 docker-compose pull
 
-docker-compose 
+docker-compose up -d
+
+docker-compose exec -T workspace php artisan key:generate
+
+docker-compose exec -T workspace composer install -q --no-ansi --no-interaction --no-scripts --no-progress --prefer-dist
+
+# php artisan config:cache
+# php artisan storage:link
+# php artisan route:cache
+# php artisan view:cache
+# php artisan horizon:terminate
+# php artisan config:cache && php artisan route:cache && php artisan view:cache && php artisan horizon:terminate
+
+# echo 'export DOCKER_USER="$(id -u):$(id -g)"' >> ~/.bash_profile
+# source ~/.bash_profile
+
+# docker-compose exec -T workspace php artisan migrate:fresh --force
+
+
+
+
