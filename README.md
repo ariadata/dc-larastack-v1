27dfa5550a6b39cb239b586ff35c85cb3aad7890
Mehrdad Amini <mehrdad@arzinja.dev>
2022-07-01 16:55:00 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# larastack-v1
-Laravel Docker-Compose Stack
+# dc-larastack v1
+[![Build Status](https://files.ariadata.co/file/ariadata_logo.png)](https://ariadata.co)
+
+![](https://img.shields.io/github/stars/ariadata/dc-larastack-v1.svg)
+![](https://img.shields.io/github/watchers/ariadata/dc-larastack-v1.svg)
+![](https://img.shields.io/github/forks/ariadata/dc-larastack-v1.svg)
+
+### This Stack needs : 
+
+* for `Local` : [dockerhost](https://github.com/ariadata/ubuntu-sh)
+
+* for `Production` : [dockerhost](https://github.com/ariadata/ubuntu-sh) + [Nginx-Proxy-Manager](https://github.com/ariadata/dc-nginxproxymanager)
+
+---
+## 🧪 **Local**
+### Install and initialize
+```bash
+git clone https://github.com/ariadata/dc-larastack-v1.git dc-larastack-v1 && cd dc-larastack-v1
+
+sed -i "s|UID=.*|UID=$(id -u)|g" .env && sed -i "s|GID=.*|GID=$(id -g)|g" .env
+sed -i "s|DC_TZ=.*|DC_TZ=\"$(cat /etc/timezone)\"|g" .env
+sed -i "s|APP_TIMEZONE=.*|APP_TIMEZONE=\"$(cat /etc/timezone)\"|g" ./src/.env
+sed -i "s|php_value\[date.timezone\] \=.*|php_value\[date.timezone\] \= $(cat /etc/timezone)|g" ./configs/php/fpm-php-override.ini
---
+++ v1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+clear
+cd "$(dirname "$0")"
+
+system_default_timezone="$(cat /etc/timezone)"
+STACK_UID="$(id -u)"
+STACK_GID="$(id -g)"
+
+read -e -p $'Enter \e[33mStack Name\033[0m :\n' -i "larastack" DC_COMPOSE_PROJECT_NAME
+
+read -e -p $'Enter \e[33mdefault timezone\033[0m for stack :\n' -i $system_default_timezone STACK_DEFAULT_TZ
+
+read -e -p $'Enter \e[33mnginx http port\033[0m for stack :\n' -i "8051" STACK_NGINX_HTTP_PORT
+
+read -e -p $'Enter \e[33mPHPMyAdmin http port\033[0m for stack :\n' -i "8082" STACK_PHPMYADMIN_HTTP_PORT
+
+mkdir -p ./data/{mongo,mysql,pgsql,redis}
+cp .env.example .env
+cp ./src/.env.example ./src/.env
+
+# set compose_project_name .env
+sudo sed -i "s|COMPOSE_PROJECT_NAME=.*|COMPOSE_PROJECT_NAME=$DC_COMPOSE_PROJECT_NAME|g" .env
+
+# set UID/GID to .env
+sudo sed -i "s|UID=.*|UID=$STACK_UID|g" .env && sed -i "s|GID=.*|GID=$STACK_GID|g" .env
+
+# set Default time zone in .env and /src/.env and php_overrides
+sudo sed -i "s|DC_TZ=.*|DC_TZ=\"$STACK_DEFAULT_TZ\"|g" .env
+sudo sed -i "s|APP_TIMEZONE=.*|APP_TIMEZONE=\"$STACK_DEFAULT_TZ\"|g" ./src/.env
+sudo sed -i "s|php_value\[date.timezone\] \=.*|php_value\[date.timezone\] \= $STACK_DEFAULT_TZ|g" ./configs/php/fpm-php-override.ini
+
+# set http port for nginx and phpmyadmin
+sudo sed -i "s|DC_PHPMYADMIN_HTTP_PORT=.*|DC_PHPMYADMIN_HTTP_PORT=$STACK_NGINX_HTTP_PORT|g" .env
+sudo sed -i "s|DC_NGINX_HTTP_PORT=.*|DC_NGINX_HTTP_PORT=$STACK_PHPMYADMIN_HTTP_PORT|g" .env
+
+cp local.yml docker-compose.yml
